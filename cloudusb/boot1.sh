ab97b3b38db82543dee284167f044410fc849369
lch01387 <lch01387@gmail.com>
2017-07-31 21:03:40 +0900

    modify readme

--- cloudusb/boot1.sh
+++ cloudusb/boot1.sh
@@ -2,4 +2,4 @@
 
 sh /home/pi/for_pi/cloudusb/communicate_module/makenode.sh
 sudo insmod /home/pi/for_pi/cloudusb/communicate_module/connModule.ko
-/home/pi/cloudeusb/MYFAT/MYFAT/MYFAT/main.out &
+/home/pi/MyCloudUSB/MYFAT/MYFAT/MYFAT/main.out &
